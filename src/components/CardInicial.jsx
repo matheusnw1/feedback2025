39ba12ef2f2a3ebbcc88a048ac2e59ccf14967e0
Matheus <matheus.wenceslau@eaportal.org>
2025-04-15 19:41:51 -0300

    mudança da função handleMudarNota para o componente InputButton já que é responsabilidade dele

--- src/components/CardInicial.jsx
+++ src/components/CardInicial.jsx
@@ -1,7 +1,8 @@
 import iconStar from "../assets/icon-star.svg"
 import { InputButton } from "./InputButton"
 
-export function CardInicial({ handleMudarNotaAvaliacao, handleSubmit }) {
+export function CardInicial({ setNotaAvaliacao, handleSubmit }) {
+
     return (
         <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
             <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
@@ -13,11 +14,11 @@ export function CardInicial({ handleMudarNotaAvaliacao, handleSubmit }) {
             <p className="text-sm text-light-grey mb-6 leading-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
         
             <div className="flex justify-between mb-6">
-                <InputButton handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} value={1} />
-                <InputButton handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} value={2} />
-                <InputButton handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} value={3} />
-                <InputButton handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} value={4} />
-                <InputButton handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} value={5} />
+                <InputButton setNotaAvaliacao={setNotaAvaliacao} value={1} />
+                <InputButton setNotaAvaliacao={setNotaAvaliacao} value={2} />
+                <InputButton setNotaAvaliacao={setNotaAvaliacao} value={3} />
+                <InputButton setNotaAvaliacao={setNotaAvaliacao} value={4} />
+                <InputButton setNotaAvaliacao={setNotaAvaliacao} value={5} />
             </div>
     
             <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange cursor-pointer">Submit</button>
